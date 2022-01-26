8caf124c7797167cfca3116095e15228216f7194
donikamj <67651525+donikamj@users.noreply.github.com>
2022-01-26 18:24:06 +0100

    Update multithreaded.c

--- multithreaded.c
+++ multithreaded.c
@@ -1,1 +1,25 @@
+//Donika
+#include<stdio.h>
+#include<stdlib.h>
+//perfshirja e librarise pthread.h
+#include<pthread.h>
 
+int arr[50], i;
+int n;
+
+/*
+ average, minimum, and maximum 
+ vlerat do te ruhen globalisht
+*/
+float average;
+int minimum;
+int maximum;
+
+//thread1 per kalklulimit e avarage(mesatares)
+void *th()
+{
+        int sum=0;
+        for(i=1;i<=n;i++)
+                sum=sum+arr[i];
+        average=sum/n; 
+}
